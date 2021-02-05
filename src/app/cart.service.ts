11341e8fd07955f7da664088d4e3751d7a3d0e75
manjunath-muniraju <manjunathm3@gmail.com>
2021-02-05 12:12:28 +0530

    storing products in local storage feature on ngx-webstore

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,27 +1,36 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { LOCAL_STORAGE, StorageService } from "ngx-webstorage-service";
 
 @Injectable()
-export class CartService {
+export class CartService implements OnInit {
 
   items = [];
   constructor(
+    @Inject(LOCAL_STORAGE) private storage: StorageService,
     private http: HttpClient
   ) { }
+
+  ngOnInit(){
+    this.items = this.storage.get('myCart') ? this.storage.get('myCart') : [];
+  }
 
 //Adding data to cart
   addToCart(product){
     this.items.push(product);
+    this.storage.set("myCart", this.items);
   }
 
 //getting data from the cart
   getItems(){
+    this.storage.get("myCart");
     return this.items;
   }
 
 //clear the cart data
   clearCart(){
     this.items = [];
+    this.storage.remove("myCart");
     return this.items;
   }
 
